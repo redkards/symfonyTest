3dbf4910ffb2f3c1e45db9c73381b116703cb3c6
jeremy duhem <redkardss@gmail.com>
2024-06-19 10:30:56 +0200

    creation dossier

---
+++ firstproject/src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\component\routing\annotation\route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController{
+
+    #[Route('/','home.index', methods:['GET'])]
+    public function index():Response{
+
+        return $this->render('home.html.twig', );
+           
+     
+
+    }
+       
+    
+}
+
+?>
