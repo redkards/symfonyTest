0e7af7701509d1557b9b794d7e8cd9f1f0cea3f0
jeremy duhem <redkardss@gmail.com>
2024-06-19 16:09:51 +0200

    ajout exo7

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IngredientController extends AbstractController
+{
+    #[Route('/ingredient', name: 'app_ingredient')]
+    public function index(IngredientRepository $ingredientRepository,PaginatorInterface $paginator,Request $request): Response
+    {
+        $ingredients = 
+        $ingredients = $paginator->paginate(
+            $ingredientRepository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+        return $this->render('pages/ingredient/index.html.twig', [
+            'ingredients' => $ingredients
+        ]);
+    }
+}
