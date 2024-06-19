c1dd22fc17479c71b4830b076c32efdea825f8ab
jeremy duhem <redkardss@gmail.com>
2024-06-19 14:14:23 +0200

    ajout exo6

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\Length(
+        min: 2,
+        max: 50,
+        minMessage: 'Le nom doit faire au moins {{ limit }} caractères',
+        maxMessage: 'Le nom doit faire au plus {{ limit }} caractères'
+    ) ]
+    #[Assert\NotBlank()]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    #[Assert\Positive()]
+    #[Assert\LessThan(200)]
+    #[Assert\NotNull()]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    #[Assert\NotNull()]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+    public function  __construct(){
+        $this->createdAt = new \DateTimeImmutable();
+    }
+}
