4ba389cc712de784031a90c8c4719ec8cd66385b
jeremy duhem <redkardss@gmail.com>
2024-06-20 16:26:28 +0200

    modif recettes

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use Faker\Generator;
 use Faker\Factory;
 use App\Entity\Ingredient;
-use App\Entity\Recettes;
+use App\Entity\Recette;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -25,14 +25,31 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+
+        //data ingredient
+        $ingredient =[];
         for ($i=0; $i<50 ; $i++){
        $ingredient = new Ingredient();
        $ingredient ->setName($this->faker->word())
                    ->setPrice(mt_rand(0,100));
+                   $ingredients[] = $ingredient;
                    
          $manager->persist($ingredient);
         }
-   
+   //data recette
+   for ($j=0; $j <25; $j++){
+    $recette = new Recette();
+    $recette -> setName($this->faker->word())
+    -> setTime(mt_rand(1,1440))
+    -> setNbPeople(mt_rand(0,1)==1? mt_rand(0,50) : null)
+    -> setDifficulty(mt_rand(0,1)==1? mt_rand(1,5) : null)
+    ->setDescription($this->faker->text(300))
+    ->setPrice(mt_rand(0,1)==1? mt_rand(1,1000) : null)
+    ->setIsFavorite(mt_rand(0,1)==1? true:false);
+
+   }
+
+
 $manager->flush();
     }
 }
